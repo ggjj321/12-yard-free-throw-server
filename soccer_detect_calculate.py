ffd788c6edf6456ae146fdf434aab961bdacfaa7
ggjj321 <10370324lsjh@gmail.com>
2023-08-10 15:24:42 +0800

    Feat : draw goal skelation

--- soccer_detect_calculate.py
+++ soccer_detect_calculate.py
@@ -1,34 +1,91 @@
 import cv2
 import torch
-import math
-import numpy as np
-import time
 
 mps_device = torch.device("mps")
 model = torch.hub.load('ultralytics/yolov5', 'custom', path='best.pt')
 model.to(mps_device)
 
-goal_upper_left_x  = []
-goal_upper_left_y  = []
-goal_upper_right_x = []
-goal_upper_right_y = []
-goal_down_left_x  = []
-goal_down_left_y  = []
-goal_down_right_x = []
-goal_down_right_y = []
+class DetectionObject():
+    def __init__(self):
+        self.confidence = None
+        self.x_min = None
+        self.x_max = None
+        self.y_min = None
+        self.y_max = None  
+        self.center_x = None
+        self.center_y = None
+
+    def set_data(self, confidence, x_min, x_max, y_min, y_max):
+        self.confidence = confidence
+        self.x_min = int(x_min)
+        self.x_max = int(x_max)
+        self.y_min = int(y_min)
+        self.y_max = int(y_max)
+        self.center_x = int((self.x_max + self.x_min) / 2)
+        self.center_y = int((self.y_max + self.y_min) / 2)
 
 def soccerDetectAndDraw(img):
     results = model(img)
     df = results.pandas().xyxy[0]
 
-    print(df)
+    detect_object = {
+        "goal" : DetectionObject(),
+        "ball" : DetectionObject(),
+        "top_left" : DetectionObject(),
+        "top_right" : DetectionObject(),
+        "down_left" : DetectionObject(),
+        'down_right' : DetectionObject()
+    }
 
-    for data_index in range(len(df)):
+    for od_data_index in range(len(df)):
+        object_name = df.loc[od_data_index, "name"]
+        object_confidence = df.loc[od_data_index, "confidence"]
+        if detect_object[object_name].confidence == None or float(object_confidence) > detect_object[object_name].confidence:
+            detect_object[object_name].set_data(    float(df.loc[od_data_index, "confidence"]),
+                                                    df.loc[od_data_index, "xmin"],
+                                                    df.loc[od_data_index, "xmax"],
+                                                    df.loc[od_data_index, "ymin"],
+                                                    df.loc[od_data_index, "ymax"],
+                                                )
+    if detect_object["goal"].confidence != None:
+        if detect_object["top_left"].confidence != None:
+            top_left = (detect_object["top_left"].center_x, detect_object["top_left"].center_y)
+        else:
+            top_left = (detect_object["goal"].x_min, detect_object["goal"].y_min)
+        
+        if detect_object["top_right"].confidence != None:
+            top_right = (detect_object["top_right"].center_x, detect_object["top_right"].center_y)
+        else:
+            top_right = (detect_object["goal"].x_max, detect_object["goal"].y_min)
+        
+        if detect_object["down_left"].confidence != None:
+            down_left = (detect_object["down_left"].center_x, detect_object["down_left"].center_y)
+        else:
+            down_left = (detect_object["goal"].x_min, detect_object["goal"].y_max)
+        
+        if detect_object["down_right"].confidence != None:
+            down_right = (detect_object["down_right"].center_x, detect_object["down_right"].center_y)
+        else:
+            down_right = (detect_object["goal"].x_max, detect_object["goal"].y_max)
 
-        if (float(df.at[data_index, 'confidence']) > 0.4):
-            start_point = (int(df.at[data_index, 'xmin']), int(df.at[data_index, 'ymin']))
-            end_point = (int(df.at[data_index, 'xmax']), int(df.at[data_index, 'ymax']))
+        radius = 5
+        color = (0, 0, 255)
+        thickness = -1
 
-            img = cv2.rectangle(img, start_point, end_point, (255, 255, 0), 2)
+        cv2.circle(img, top_left, radius, color, thickness)
+        cv2.circle(img, top_right, radius, color, thickness)
+        cv2.circle(img, down_left, radius, color, thickness)
+        cv2.circle(img, down_right, radius, color, thickness)
+
+        color = (0, 255, 0) 
+        thickness = 2 
+        
+        cv2.line (img, top_left, top_right, color, thickness)
+        cv2.line (img, top_right, down_right, color, thickness)
+        cv2.line (img, down_right, down_left, color, thickness)
+        cv2.line (img, down_left, top_left, color, thickness)
+
+        
+
 
     return img
